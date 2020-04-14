8b2b8d03f4ef64722f0ca5324096ab6c130c388f
Veronika K <veronikakuzn99@gmail.com>
2020-04-14 17:23:46 +0300

    Initial Commit

---
+++ FSMonitor/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FSMonitor
+//
+//  Created by Veronika on 4/14/20.
+//  Copyright © 2020 Veronika. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
